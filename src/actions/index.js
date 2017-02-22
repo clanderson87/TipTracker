18a332aa74154d734bf1b9f05b0d4e7ea28fdc86
Chris Anderson <cl.anderson8@gmail.com>
2017-02-21 19:40:10 -0600

    added types.js and named export convention

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,10 @@
-export const emailChanges = (text) => {
+import { 
+  EMAIL_CHANGED
+} from './types';
+
+export const emailChanged = (text) => {
   return {
-    type: 'email_changed',
+    type: EMAIL_CHANGED,
     payload: text
   };
 };
