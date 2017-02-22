b319da256bd455192e34a1bba87f8c93229d3726
Chris Anderson <cl.anderson8@gmail.com>
2017-02-21 18:27:49 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        
+      </Provider>
+    )
+  }
+}
