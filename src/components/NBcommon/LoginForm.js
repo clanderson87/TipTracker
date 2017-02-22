a4998d1955c1188210f38cc3e397387313e8b5b5
Chris Anderson <cl.anderson8@gmail.com>
2017-02-21 20:05:41 -0600

    password input reduxified

--- src/components/NBcommon/LoginForm.js
+++ src/components/NBcommon/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card, InputGroup, Input, Button, Text } from 'native-base';
 
-import { emailChanged } from '../../actions'
+import { emailChanged, passwordChanged } from '../../actions'
 
 class LoginForm extends Component {
   
@@ -22,6 +22,8 @@ class LoginForm extends Component {
             secureTextEntry
             label='password' 
             placeholder='password' 
+            onChangeText={text => this.props.passwordChanged(text)}
+            value={this.props.password}
           />
         </InputGroup>
         <Button block>
@@ -34,8 +36,9 @@ class LoginForm extends Component {
 
 const mapStateToProps = state => {
   return {
-    email: state.auth.email
+    email: state.auth.email,
+    password: state.auth.password
   };
 };
 
-export default connect(mapStateToProps, {emailChanged})(LoginForm);
+export default connect(mapStateToProps, {emailChanged, passwordChanged})(LoginForm);
