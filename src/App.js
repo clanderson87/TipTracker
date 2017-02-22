8c32bec260061220163cbbd2c7c2b137e662f634
Chris Anderson <cl.anderson8@gmail.com>
2017-02-21 18:38:57 -0600

    initial boilerplate minus firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
-
+import { 
+  Container, 
+  Header,
+  Title,
+  Content,
+  Body,
+  Footer
+} from 'native-base';
+import { Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
 
 class App extends Component {
   render() {
     return (
-      <Provider>
-        
+      <Provider store={createStore()}>
+        <Container>
+          <Header>
+            <Title>
+              Main Title!
+            </Title>
+          </Header>
+          <Content>
+            <Text>
+              Main Text!
+            </Text>
+          </Content>
+          <Footer>
+            <Text>
+              Main footer!
+            </Text>
+          </Footer>
+        </Container>
       </Provider>
     )
   }
 }
+
+export default App;
