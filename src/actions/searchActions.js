be8ff6fd635b169165c6b639cdaecd13b2d437aa
Chris Anderson <cl.anderson8@gmail.com>
2017-02-23 15:13:41 -0600

    installed react-native-google-autocomplete

---
+++ src/actions/searchActions.js
@@ -0,0 +1,3 @@
+import { Actions } from 'react-native-router-flux';
+
+import GOOGLE_PLACES_API_KEY from '../../secrets/GOOGLE_PLACES_API_KEY';
--- src/components/RestaurantCreate.js
+++ src/components/RestaurantCreate.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Card, Text, InputGroup, Input, Button } from 'native-base';
 
-import GOOGLE_PLACES_API_KEY from '../../secrets/GOOGLE_PLACES_API_KEY';
-
 class RestaurantCreate extends Component {
   render() {
     return(
